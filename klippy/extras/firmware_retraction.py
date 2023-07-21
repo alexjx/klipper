5d71d97de293d3cf2edb059dd2807e5795d7656f
Jia Xin <jia.xin@fastonetech.com>
2023-07-21 11:49:30 +0800

    Revert "feat: naive zhop during firmware retraction"
    This is bad since https://github.com/Klipper3d/klipper/issues/1787
    
    This reverts commit 2ae7eddd1dd4baf4307340ae90d4c489f165b0e2.

--- klippy/extras/firmware_retraction.py
+++ klippy/extras/firmware_retraction.py
@@ -14,9 +14,7 @@ class FirmwareRetraction:
         self.unretract_speed = config.getfloat('unretract_speed', 10., minval=1)
         self.unretract_length = (self.retract_length
                                  + self.unretract_extra_length)
-        self.zhop = 0.0
         self.is_retracted = False
-        self.last_zhop = 0.0  # just in case we change the zhop during a retraction
         self.gcode = self.printer.lookup_object('gcode')
         self.gcode.register_command('SET_RETRACTION', self.cmd_SET_RETRACTION,
                                     desc=self.cmd_SET_RETRACTION_help)
@@ -31,7 +29,6 @@ class FirmwareRetraction:
             "retract_speed": self.retract_speed,
             "unretract_extra_length": self.unretract_extra_length,
             "unretract_speed": self.unretract_speed,
-            "zhop": self.zhop,
         }
     cmd_SET_RETRACTION_help = ("Set firmware retraction parameters")
     def cmd_SET_RETRACTION(self, gcmd):
@@ -45,46 +42,33 @@ class FirmwareRetraction:
                                               self.unretract_speed, minval=1)
         self.unretract_length = (self.retract_length
                                  + self.unretract_extra_length)
-        self.zhop = gcmd.get_float('ZHOP', self.zhop, minval=0., maxval=10.)
         self.is_retracted = False
     cmd_GET_RETRACTION_help = ("Report firmware retraction paramters")
     def cmd_GET_RETRACTION(self, gcmd):
         gcmd.respond_info("RETRACT_LENGTH=%.5f RETRACT_SPEED=%.5f"
                           " UNRETRACT_EXTRA_LENGTH=%.5f UNRETRACT_SPEED=%.5f"
-                          " ZHOP=%.5f"
                           % (self.retract_length, self.retract_speed,
-                             self.unretract_extra_length, self.unretract_speed,
-                             self.zhop))
+                             self.unretract_extra_length, self.unretract_speed))
 
     def cmd_G10(self, gcmd):
         if not self.is_retracted:
-            cmds = [
-                "SAVE_GCODE_STATE NAME=_retract_state",
-                "G91",
-            ]
-            cmds.append("G1 E-%.5f F%d" % (self.retract_length, self.retract_speed * 60,))
-            if self.zhop > 0.0:
-                cmds.append("G1 Z%.5f F1200" % (self.zhop,))
-                self.last_zhop = self.zhop
-            cmds.append("RESTORE_GCODE_STATE NAME=_retract_state")
-            self.gcode.run_script_from_command("\n".join(cmds))
+            self.gcode.run_script_from_command(
+                "SAVE_GCODE_STATE NAME=_retract_state\n"
+                "G91\n"
+                "G1 E-%.5f F%d\n"
+                "RESTORE_GCODE_STATE NAME=_retract_state"
+                % (self.retract_length, self.retract_speed*60))
             self.is_retracted = True
 
     def cmd_G11(self, gcmd):
         if self.is_retracted:
-            cmds = [
-                "SAVE_GCODE_STATE NAME=_retract_state",
-                "G91",
-            ]
-            cmds.append("G1 E%.5f F%d" % (self.retract_length, self.retract_speed * 60,))
-            if self.last_zhop > 0.0:
-                cmds.append("G1 Z-%.5f F1200" % (self.last_zhop,))
-                self.last_zhop = 0.0
-            cmds.append("RESTORE_GCODE_STATE NAME=_retract_state")
-            self.gcode.run_script_from_command("\n".join(cmds))
+            self.gcode.run_script_from_command(
+                "SAVE_GCODE_STATE NAME=_retract_state\n"
+                "G91\n"
+                "G1 E%.5f F%d\n"
+                "RESTORE_GCODE_STATE NAME=_retract_state"
+                % (self.unretract_length, self.unretract_speed*60))
             self.is_retracted = False
 
 def load_config(config):
     return FirmwareRetraction(config)
-
-
