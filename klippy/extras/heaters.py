209cbb7521004cbf33252fb90fdb3b7d7986df12
Jia Xin <alexjx@gmail.com>
2023-10-10 22:16:33 +0800

    fix: correct set_heater_min_extruder_temp command

--- klippy/extras/heaters.py
+++ klippy/extras/heaters.py
@@ -61,9 +61,9 @@ class Heater:
         gcode.register_mux_command("SET_HEATER_TEMPERATURE", "HEATER",
                                    self.name, self.cmd_SET_HEATER_TEMPERATURE,
                                    desc=self.cmd_SET_HEATER_TEMPERATURE_help)
-        gcode.register_mux_command("SET_HEATER_MIN_TEMP", "HEATER",
-                                   self.name, self.cmd_SET_HEATER_MIN_TEMP,
-                                   desc=self.cmd_SET_HEATER_MIN_TEMP_help)
+        gcode.register_mux_command("SET_HEATER_MIN_EXTRUDE_TEMP", "HEATER",
+                                   self.name, self.cmd_SET_HEATER_MIN_EXTRUDE_TEMP,
+                                   desc=self.cmd_SET_HEATER_MIN_EXTRUDE_TEMP_help)
     def set_pwm(self, read_time, value):
         if self.target_temp <= 0.:
             value = 0.
@@ -137,17 +137,19 @@ class Heater:
             smoothed_temp = self.smoothed_temp
             last_pwm_value = self.last_pwm_value
         return {'temperature': round(smoothed_temp, 2), 'target': target_temp,
-                'power': last_pwm_value, "min_temp": self.min_temp, "max_temp": self.max_temp}
+                'power': last_pwm_value, "min_temp": self.min_temp, "max_temp": self.max_temp,
+                'min_extrude_temp': self.min_extrude_temp}
     cmd_SET_HEATER_TEMPERATURE_help = "Sets a heater temperature"
     def cmd_SET_HEATER_TEMPERATURE(self, gcmd):
         temp = gcmd.get_float('TARGET', 0.)
         pheaters = self.printer.lookup_object('heaters')
         pheaters.set_temperature(self, temp)
-    cmd_SET_HEATER_MIN_TEMP_help = "Sets the min temperature of heater"
-    def cmd_SET_HEATER_MIN_TEMP(self, gcmd):
+    cmd_SET_HEATER_MIN_EXTRUDE_TEMP_help = "Sets the min extrude temperature of heater"
+    def cmd_SET_HEATER_MIN_EXTRUDE_TEMP(self, gcmd):
         temp = gcmd.get_float('TEMP', 0.)
-        self.min_temp = temp
-        self.sensor.setup_minmax(self.min_temp, self.max_temp)
+        self.min_extrude_temp = temp
+        self.can_extrude = (self.smoothed_temp >= self.min_extrude_temp)
+        gcmd.respond_info(f"heater {self.name}: min_extrude_temp set to {self.min_extrude_temp}")
 
 
 ######################################################################
